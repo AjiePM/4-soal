055d71cdcde99f58871d7c6d4d5cd022455988e9
Ajie Prana Mesa <ajiemesa19@gmail.com>
2022-07-21 21:43:36 +0800

    first commit

---
+++ tugas algoritma.js
@@ -0,0 +1,27 @@
+// tugas 1
+function pukul (jam, detik){
+    return jam * detik
+} console.log(pukul( 2, 3600))
+
+// tugas 2
+let nilai1 = 90
+let nilai2 = 90
+
+if (nilai1 => nilai2){
+    console.log(nilai1)
+} else if (nilai2 => nilai1){
+    console.log(nilai2)
+}else {
+    console.log(nilai1 + nilai2)
+}
+
+//tugas 3
+function hargaTiket (orang,harga){
+    return orang * harga
+} console.log(hargaTiket(2, 600000))
+
+// tugas 4
+let nilai = 100
+for (nilai = 0 ; nilai < 100; nilai ++){
+
+}console.log(nilai)
